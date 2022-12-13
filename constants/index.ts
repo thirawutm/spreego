98afbe60fcf31e305dc17023f282d0853a80077d
Kornsupadej Suvarnakasa <kornsupadej@rentspree.com>
2022-12-13 15:26:41 +0700

    [🔧] add service

--- constants/index.ts
+++ constants/index.ts
@@ -1,10 +1,19 @@
 const LINE_BASE_URL = "https://api.line.me";
 const LINE_DATA_URL = "https://api-data.line.me";
+
 const Constants = {
   LINE: {
     MESSAGING_API: {
       WEBHOOK: `${LINE_BASE_URL}/v2/bot/message/{messageId}/content`,
     },
+  },
+  SCREEN_SIZE: {
+    SSS: 320,
+    SS: 600,
+    XS: 769,
+    SM: 1080,
+    MD: 1440,
+    LG: 1920,
   },
 };
 
