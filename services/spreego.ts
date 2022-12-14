a5424d7490d272642c3e751ada93b7dabd26e012
Kornsupadej Suvarnakasa <kornsupadej@rentspree.com>
2022-12-14 14:51:45 +0700

    adjust flex message

--- services/spreego.ts
+++ services/spreego.ts
@@ -30,7 +30,7 @@ export namespace SpreeGOService {
               action: {
                 type: "uri",
                 label: "เปิดตี้",
-                uri: "https://google.com",
+                uri: " https://liff.line.me/1657732696-MwOP0zVZ/event/create",
               },
             },
             {
@@ -55,190 +55,130 @@ export namespace SpreeGOService {
         altText: "สวัสดี SpreePle, มีตี้ใหม่ สนใจป่าวว 🔥",
         contents: {
           type: "bubble",
-          hero: {
-            type: "image",
-            url: "https://lh5.googleusercontent.com/p/AF1QipMSaBclL0pmNMCp43c4nb4MzT7bUgW0c5gK81Vo=w426-h240-k-no",
-            size: "full",
-            aspectRatio: "20:13",
-            aspectMode: "cover",
-            action: {
-              type: "uri",
-              label: "Join",
-              uri: "http://linecorp.com/",
-            },
-          },
-          body: {
+          header: {
             type: "box",
-            layout: "vertical",
+            layout: "baseline",
             contents: [
               {
                 type: "text",
-                text: "Basketball (3x3)",
+                text: "Basketball",
                 weight: "bold",
-                size: "xl",
-              },
-              {
-                type: "box",
-                layout: "vertical",
-                margin: "lg",
-                spacing: "sm",
-                contents: [
-                  {
-                    type: "box",
-                    layout: "horizontal",
-                    spacing: "sm",
-                    contents: [
-                      {
-                        type: "text",
-                        text: "Place",
-                        color: "#aaaaaa",
-                        size: "sm",
-                        flex: 1,
-                      },
-                      {
-                        type: "text",
-                        wrap: true,
-                        color: "#666666",
-                        size: "sm",
-                        flex: 4,
-                        contents: [],
-                        text: "The Street Ratchada,\nRatchadaphisek Rd, Din Daeng, Bangkok 10400" as unknown as undefined,
-                      },
-                    ],
-                  },
-                  {
-                    type: "box",
-                    layout: "baseline",
-                    spacing: "sm",
-                    contents: [
-                      {
-                        type: "text",
-                        text: "Date",
-                        color: "#aaaaaa",
-                        size: "sm",
-                        flex: 1,
-                      },
-                      {
-                        type: "text",
-                        text: "Tuesday 20 Dec 2022",
-                        wrap: true,
-                        color: "#666666",
-                        size: "sm",
-                        flex: 4,
-                      },
-                    ],
-                  },
-                  {
-                    type: "box",
-                    layout: "baseline",
-                    spacing: "sm",
-                    contents: [
-                      {
-                        type: "text",
-                        text: "Time",
-                        color: "#aaaaaa",
-                        size: "sm",
-                        flex: 1,
-                      },
-                      {
-                        type: "text",
-                        text: "20:00 - 22:00",
-                        wrap: true,
-                        color: "#666666",
-                        size: "sm",
-                        flex: 4,
-                      },
-                    ],
-                  },
-                  {
-                    type: "box",
-                    layout: "horizontal",
-                    contents: [
-                      {
-                        type: "text",
-                        color: "#aaaaaa",
-                        size: "xs",
-                        text: "Joiners",
-                        margin: "none",
-                        align: "start",
-                        flex: 1,
-                      },
-                      {
-                        type: "box",
-                        layout: "baseline",
-                        contents: [
-                          {
-                            type: "icon",
-                            url: "https://gitlab.com/uploads/-/system/user/avatar/9296008/avatar.png",
-                          },
-                          {
-                            type: "icon",
-                            url: "https://gitlab.com/uploads/-/system/user/avatar/11723687/avatar.png",
-                          },
-                          {
-                            type: "icon",
-                            url: "https://gitlab.com/uploads/-/system/user/avatar/9416234/avatar.png",
-                          },
-                          {
-                            type: "icon",
-                            url: "https://gitlab.com/uploads/-/system/user/avatar/10645646/avatar.png",
-                          },
-                          {
-                            type: "text",
-                            wrap: true,
-                            color: "#666666",
-                            size: "sm",
-                            text: "and 12 joiners",
-                          },
-                        ],
-                        flex: 4,
-                        spacing: "md",
-                        offsetStart: "5px",
-                      },
-                    ],
-                  },
-                ],
+                size: "xxl",
+                color: "#3371FF",
               },
             ],
           },
-          footer: {
+          body: {
             type: "box",
             layout: "vertical",
             spacing: "sm",
             contents: [
               {
-                type: "button",
-                style: "link",
-                height: "sm",
-                action: {
-                  type: "uri",
-                  label: "GOOGLE MAPS",
-                  uri: "https://www.google.com/maps/place/THE+STREET+RATCHADA/@13.7705118,100.5722782,15z/data=!4m2!3m1!1s0x0:0xb0e50b499216c2d7?sa=X&ved=2ahUKEwiCj9uYjeX7AhWjSGwGHWvHAtEQ_BJ6BAhnEAg",
-                },
+                type: "box",
+                layout: "horizontal",
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "text",
+                    text: "Place",
+                    color: "#aaaaaa",
+                    size: "sm",
+                    flex: 1,
+                  },
+                  {
+                    type: "text",
+                    wrap: true,
+                    color: "#666666",
+                    size: "sm",
+                    flex: 4,
+                    contents: [],
+                    text: "The Street Ratchada,\nRatchadaphisek Rd, Din Daeng, Bangkok 10400" as unknown as undefined,
+                  },
+                ],
               },
               {
-                type: "button",
-                style: "secondary",
-                height: "sm",
-                action: {
-                  type: "uri",
-                  label: "DETAILS",
-                  uri: "https://linecorp.com",
-                },
+                type: "box",
+                layout: "baseline",
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "text",
+                    text: "Date",
+                    color: "#aaaaaa",
+                    size: "sm",
+                    flex: 1,
+                  },
+                  {
+                    type: "text",
+                    text: "Tuesday 20 Dec 2022",
+                    wrap: true,
+                    color: "#666666",
+                    size: "sm",
+                    flex: 4,
+                  },
+                ],
               },
               {
-                type: "button",
-                action: {
-                  type: "postback",
-                  label: "JOIN NOW",
-                  data: "activity-id",
-                  displayText: "ไปด้วย",
-                },
-                style: "primary",
-                color: "#004bb0",
+                type: "box",
+                layout: "baseline",
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "text",
+                    text: "Time",
+                    color: "#aaaaaa",
+                    size: "sm",
+                    flex: 1,
+                  },
+                  {
+                    type: "text",
+                    text: "20:00 - 22:00",
+                    wrap: true,
+                    color: "#666666",
+                    size: "sm",
+                    flex: 4,
+                  },
+                ],
+              },
+              {
+                type: "box",
+                layout: "vertical",
+                margin: "xl",
+                spacing: "sm",
+                backgroundColor: "#3371FF",
+                cornerRadius: "md",
+                contents: [
+                  {
+                    type: "button",
+                    color: "#FFFFFF",
+                    action: {
+                      type: "uri",
+                      label: "Join",
+                      uri: "https://google.com",
+                    },
+                  },
+                ],
+              },
+              {
+                type: "box",
+                layout: "vertical",
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "button",
+                    color: "#FF0000",
+                    action: {
+                      type: "uri",
+                      label: "Adjust Event",
+                      uri: "https://google.com",
+                    },
+                  },
+                ],
+              },
+              {
+                type: "separator",
               },
             ],
-            flex: 0,
           },
         },
       },
