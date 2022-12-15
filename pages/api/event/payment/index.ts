396b89778686f641c50737fdddddc755ae0b1445
Thirawut Muninta <thirawut@rentspree.com>
2022-12-15 19:50:28 +0700

    build payment api

---
+++ pages/api/event/payment/index.ts
@@ -0,0 +1,124 @@
+import clientPromise from "../../../../lib/mongodb"
+
+import type { NextApiRequest, NextApiResponse } from "next"
+import Constants from "../../../../constants"
+import { Document, ObjectId } from "mongodb"
+import { SpreeGOService } from "../../../../services/spreego"
+
+const COLLECTION_NAME = "events"
+
+const list = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  collection: Document
+) => {
+  const events = await collection.find().toArray()
+  
+  return res.json({ status: true, events })
+}
+
+const create = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  collection: Document
+) => {
+  const { eventId,
+  paymentPromptPay,
+  totalMoney } = req.body
+
+  const event = await collection.findOne({ _id: new ObjectId(eventId) })
+
+  if (!event)
+    res.status(404).json({ status: false, message: "Event is not found" })
+
+  if (event.status === false) {
+    res.status(500).json({ status: false, message: "Event is deleted" })
+  }
+
+  if (event.isCompleted !== true) {
+    res.status(500).json({ status: false, message: "Event has not completed yet" })
+  }
+  
+  const { members} = event
+
+  const peopleJoin = members.filter((member: any) => member.joinType==='going')
+  if(peopleJoin.length === 0) {
+    return res.status(500).json({status: false, message: "There is no joining people"})
+  }
+
+    const totalJoin = peopleJoin.reduce((total: number, member: any) => {
+      return (total +=
+        1 + (parseInt(member.withFriends?.toString() ?? "0") ?? 0))
+    }, 0)
+
+  const moneyPerPeople = Math.round(parseFloat(totalMoney) / totalJoin)
+
+  const updateMembers = members.map((member: any) => {
+    if(member.joinType === 'going') {
+      member.money = (1 + parseInt(member.withFriends ?? "0")) * moneyPerPeople
+    }
+    return member
+  })
+
+  const payment = {
+    paymentPromptPay,
+    totalMoney
+  }
+
+  const updated = await collection.updateOne(
+    { _id: new ObjectId(eventId) },
+    { $set: { members: updateMembers, payment, updatedAt: new Date() } }
+  )
+
+
+  // await SpreeGOService.payment(event)
+
+  return res.json({ status: true, updated })
+
+}
+
+const update = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  collection: Document
+) => {
+  const { id, ...toUpdate } = req.body
+
+  const updated = await collection.updateOne(
+    { _id: new ObjectId(id) },
+    { $set: { ...toUpdate, updatedAt: new Date() } }
+  )
+
+  return res.json({ status: true, updated })
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const client = await clientPromise
+    const db = client.db(Constants.DB.NAME)
+
+    const collection = db.collection(COLLECTION_NAME)
+
+    console.log(req.method)
+    switch (req.method) {
+    //   case "GET":
+    //     return list(req, res, collection)
+      case "POST":
+        return create(req, res, collection)
+    //   case "PUT":
+    //     return update(req, res, collection)
+      default:
+        return res
+          .status(404)
+          .json({ status: false, message: "Http method not found" })
+    }
+  } catch (e) {
+    console.error(e)
+    return res
+      .status(500)
+      .json({ status: false, message: "Internal server error" })
+  }
+}
